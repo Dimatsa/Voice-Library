87c8f1c92b71189ad3c9c966e16460789464a859
Carmen <carmen.lamprecht1@gmail.com>
2021-01-16 16:05:13 -0500

    fixed colours

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,21 +2,46 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledAppDiv = styled.div`
-  text-align: center;
-
   .Login-header {
-    background-color: #6fcf97;
-    height: 386px;
+    background-color: #2f80ed;
+    float: right;
+    height: 1000px;
+    width: 1050px;
     padding: 20px;
     color: #333333;
+  }
+
+  .button {
+    font-size: 13px;
+    margin-top: 800px;
+    color: #00acf5;
   }
 
   .Login-intro {
     position: absolute;
     width: 824px;
     height: 215px;
-    left: 620px;
+    left: 30px;
     top: 0px;
+    color: #e5e5e5;
+
+    font-family: sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 144px;
+    line-height: 184px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+  }
+
+  .Login-intro2 {
+    position: absolute;
+    width: 824px;
+    height: 215px;
+    left: 265px;
+    top: 0px;
+    color: #5cbbff;
 
     font-family: sans-serif;
     font-style: normal;
@@ -29,18 +54,23 @@ const StyledAppDiv = styled.div`
   }
 
   .Login-subtitle {
-    position: absolute;
-    left: 620px;
-    top: 250px;
+    /* YouSpeak, so you don’t have to. */
 
-    font-family: sans-serif;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 32px;
-    line-height: 184px;
+    position: absolute;
+    width: 700px;
+    height: 112px;
+    left: 800px;
+    top: 296px;
+
+    font-family: Raleway;
+    font-style: sans-serif;
+    font-weight: 840;
+    font-size: 80px;
+    line-height: 80px;
     display: flex;
-    align-items: center;
-    text-align: center;
+    align-items: left;
+
+    color: #ffffff;
   }
 `;
 
@@ -49,8 +79,9 @@ export default () => {
     <>
       <StyledAppDiv>
         <div className="Login-header"></div>
-        <p className="Login-intro">YouSpeak</p>
-        <p className="Login-subtitle">YouSpeak so you don&#39;t have to</p>
+        <p className="Login-intro">You</p>
+        <p className="Login-intro2">Speak</p>
+        <p className="Login-subtitle">YouSpeak, so you don&#39;t have to</p>
         <button>Login</button>
       </StyledAppDiv>
     </>
