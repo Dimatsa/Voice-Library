2e6bcbf770b2306857d717ce9ea49bd085f44b75
Ryan Larkin <eandr127@outlook.com>
2021-01-16 10:52:49 -0500

    Switch to styled components and autoformat

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # HTN-2021
 
 ## Initial Setup
+
 1. Ensure `node` and `npm` are installed
 1. `npm install`
 
 ## Initial Google Cloud Setup
+
 1. Install using these instructions: https://cloud.google.com/sdk/docs/quickstart
 1. `gcloud auth login` and follow the instructions
 1. `gcloud config set project [Project ID]`
 
 ## Running locally
+
 1. `npm start`
 
 ## Building and deploying
+
 1. `npm run build`
 1. `gcloud app deploy server/`
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,34 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import styled, { createGlobalStyle } from "styled-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'sans-serif';
+}
+`;
+
+const StyledAppDiv = styled.div`
+  text-align: center;
+
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 80px;
+  }
+
+  .App-header {
+    background-color: #222;
+    height: 150px;
+    padding: 20px;
+    color: white;
+  }
+
+  .App-intro {
+    font-size: large;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default () => {
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState("");
 
-  useEffect(() => fetch('/api/message')
-    .then(response => response.json())
-    .then(json => setMessage(json)), [])
+  useEffect(
+    () =>
+      axios
+        .get("/api/message")
+        .then((response) => response.data)
+        .then((data) => setMessage(data)),
+    []
+  );
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Router>
-          <Switch>
-            <Route path="/test">
-              <h2>{message}</h2>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  )
-}
+    <>
+      <GlobalStyle />
+      <StyledAppDiv>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Router>
+            <Switch>
+              <Route path="/test">
+                <h2>{message}</h2>
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </StyledAppDiv>
+    </>
+  );
+};
