7760b4ff8bda2d26d9f8875d388ce3b13e814e3f
Dmitry Tsarapkine <32915731+Dimatsa@users.noreply.github.com>
2021-01-16 16:09:38 -0500

    Began adding speech-to-text

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,27 +6,6 @@ router.get("/message", function (req, res) {
   res.json("Welcome To React (backend)");
 });
 
-/*var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
-});
-
-var upload = multer({ storage: storage });
-
-router.post("/uploadaudio", upload.single("audio"), function (req, res, next) {
-  const file = req.file;
-  if (!file) {
-    const error = new Error("Please upload a file");
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(file);
-});
-*/
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/uploadaudio", upload.single("audio"), function (req, res, next) {
