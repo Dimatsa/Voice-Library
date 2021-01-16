1972bdb092cd6db21cec64150db20ceb2f414c5c
Dmitry Tsarapkine <32915731+Dimatsa@users.noreply.github.com>
2021-01-15 23:20:28 -0500

    Setup Node.js

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+app.get('/', function (req, res) {
+    res.send('GET');
+});
+
+app.post('/', function (req, res) {
+    res.send('POST');
+});
