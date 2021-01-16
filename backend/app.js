3cdd5097d2734159c1087f89b1c0bcb809970adc
Dmitry Tsarapkine <32915731+Dimatsa@users.noreply.github.com>
2021-01-15 23:36:23 -0500

    Setup frontend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function () {
+app.listen(5000, function () {
     console.log('Example app listening on port 3000!');
 });
 
