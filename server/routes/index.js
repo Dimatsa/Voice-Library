6a8b43928278a2aa50563873cacfcd592185ffa8
acegiansal <50672233+acegiansal@users.noreply.github.com>
2021-01-16 16:57:18 -0500

    Splitting audio clips now possible

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,39 @@ var router = express.Router();
 const fs = require("fs");
 const multer = require("multer");
 
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
+
 router.get("/message", function (req, res) {
   res.json("Welcome To React (backend)");
 });
 
+router.get("/split-voices", (req, res) => {
+  var testVoice = "./server/counting.mp3";
+
+  function splitAudio() {}
+
+  ffmpeg.ffprobe(testVoice, (err, metaData) => {
+    //const { duration } = metaData.format;
+    //const { duration } = 2;
+    const startingTime = 2;
+    console.log(startingTime);
+    const clipDuration = 5;
+    ffmpeg()
+      .input("./server/counting.mp3")
+      .inputOptions([`-ss ${startingTime}`])
+      .outputOptions([`-t ${clipDuration}`])
+      .output("./endResult.mp3")
+      .on("end", () => console.log("done"))
+      .on("error", (err) => console.error(err))
+      .run();
+  });
+
+  res.send("hi");
+});
+
+/* Must be sent files to be merged in order */
 router.get("/combine-voices", (req, res) => {
   const testDir = "./server/ginafile/";
   var files = fs.readdirSync(testDir);
@@ -37,27 +66,7 @@ router.get("/combine-voices", (req, res) => {
   }
   combineFiles();
 });
-/*var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
-});
 
-var upload = multer({ storage: storage });
-
-router.post("/uploadaudio", upload.single("audio"), function (req, res, next) {
-  const file = req.file;
-  if (!file) {
-    const error = new Error("Please upload a file");
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(file);
-});
-*/
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/uploadaudio", upload.single("audio"), function (req, res, next) {
