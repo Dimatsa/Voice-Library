28c008385ce8351fc7f6d9fa7e8f92ff2a0bb7a7
Ryan Larkin <eandr127@outlook.com>
2021-05-13 16:59:36 -0400

    Build TS files for backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
