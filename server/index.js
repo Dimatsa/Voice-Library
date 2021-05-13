ccf2feb8e0202719bebd85a9fc0781430222987c
Ryan Larkin <eandr127@outlook.com>
2021-05-13 15:16:32 -0400

    Begin restructuring of repository

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
