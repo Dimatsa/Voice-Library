2178f7ecd2c1efd8d09f5881ac50fc44f407800d
Ryan Larkin <eandr127@outlook.com>
2021-05-13 16:28:15 -0400

    Add ESLint and Prettier support

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : "Hello " + data}</p>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
